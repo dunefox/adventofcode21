2af5c584945e91b82a73e79d3d458fad0bed60dc
Phillip Fuchs <dunefox@tuta.io>
2021-12-05 16:06:10 +0100

    Halved runtime from ~150ms to ~75ms by using type annotations with @code_warntype: Any to concrete type

--- 5/main.jl
+++ 5/main.jl
@@ -10,16 +10,16 @@ small_input = read_file("small_input.txt", r"(,)|( -> )")
 input = read_file("input.txt", r"(,)|( -> )")
 
 function part1(input)
-    counts = Dict{Array{Int}, Int}()
+    counts = Dict{Tuple{Int, Int}, Int}()
 
     for (x1, y1, x2, y2) in input
         if x1 == x2 && y1 != y2
-            for coord in range(sort([y1, y2])...)
-                counts[[x1, coord]] = get(counts, [x1, coord], 0) + 1
+            for coord::Int in range(sort([y1, y2])...)
+                counts[(x1, coord)] = get(counts, (x1, coord), 0) + 1
             end
         elseif x1 != x2 && y1 == y2
-            for coord in range(sort([x1, x2])...)
-                counts[[coord, y1]] = get(counts, [coord, y1], 0) + 1
+            for coord::Int in range(sort([x1, x2])...)
+                counts[(coord, y1)] = get(counts, (coord, y1), 0) + 1
             end
         end
     end
@@ -28,7 +28,7 @@ function part1(input)
 end
 
 @test part1(small_input) == 5
-part1(input)
+@time part1(input)
 
 # For testing purposes; getting the directions right ain't easy
 function fill_board(counts)
@@ -42,24 +42,24 @@ function fill_board(counts)
 end
 
 function part2(input)
-    counts = Dict{Array{Int}, Int}()
+    counts = Dict{Tuple{Int, Int}, Int}()
 
     for (x1, y1, x2, y2) in input
         if x1 - x2 == y1 - y2
-            for (coord1, coord2) in zip(range(sort([x1, x2])...), range(sort([y1, y2])...))
-                counts[[coord1, coord2]] = get(counts, [coord1, coord2], 0) + 1
+            for (coord1::Int, coord2::Int) in zip(range(sort([x1, x2])...), range(sort([y1, y2])...))
+                counts[(coord1, coord2)] = get(counts, (coord1, coord2), 0) + 1
             end
         elseif abs(x1 - y2) == abs(x2 - y1)
-            for (coord1, coord2) in zip(range(sort([x1, x2])...), reverse(range(sort([y1, y2])...)))
-                counts[[coord1, coord2]] = get(counts, [coord1, coord2], 0) + 1
+            for (coord1::Int, coord2::Int) in zip(range(sort([x1, x2])...), reverse(range(sort([y1, y2])...)))
+                counts[(coord1, coord2)] = get(counts, (coord1, coord2), 0) + 1
             end
         elseif x1 == x2 && y1 != y2
-            for coord in range(sort([y1, y2])...)
-                counts[[x1, coord]] = get(counts, [x1, coord], 0) + 1
+            for coord::Int in range(sort([y1, y2])...)
+                counts[(x1, coord)] = get(counts, (x1, coord), 0) + 1
             end
         elseif x1 != x2 && y1 == y2
-            for coord in range(sort([x1, x2])...)
-                counts[[coord, y1]] = get(counts, [coord, y1], 0) + 1
+            for coord::Int in range(sort([x1, x2])...)
+                counts[(coord, y1)] = get(counts, (coord, y1), 0) + 1
             end
         end
     end
