01cdaa7999a47d763e34c227b2e15a4b8c2fe269
Phillip Fuchs <dunefox@tuta.io>
2021-12-06 13:43:12 +0100

    Day 6 in Julia, part 1 & 2: Part 2 takes 109.201 μs

---
+++ 6/main.jl
@@ -0,0 +1,36 @@
+using Pkg
+Pkg.activate(".")
+
+using BenchmarkTools, Test
+
+small_input = parse.(Int8, split(readlines(open("small_input.txt"))[1], ","))
+input = parse.(Int8, split(readlines(open("input.txt"))[1], ","))
+
+function adapt_population!(population)
+    population[8] += population[1]
+    population .= circshift(population, -1)
+end
+
+function part12(input, days)
+    fishies = zeros(BigInt, 9)
+
+    map(x -> fishies[x] += 1, input .+ 1) # Julia indices are 1-based
+
+    for day in 1:days
+        adapt_population!(fishies)
+    end
+
+    return sum(fishies)
+end
+
+# Tests
+@test part12(small_input, 80) == 5934
+@test part12(input, 80) == 353274
+@test part12(small_input, 256) == 26984457539
+@test part12(input, 256) == 1609314870967
+
+# Benchmarks
+@benchmark part12(small_input, 80)
+@benchmark part12(input, 80)
+@benchmark part12(small_input, 256)
+@benchmark part12(input, 256)
