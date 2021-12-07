caa0c3f23ac23967c675b41026c09ff5bd9998fd
Phillip Fuchs <dunefox@tuta.io>
2021-12-07 18:11:45 +0100

    Day 7: Both parts in Julia; part 1 takes ~900 us, part 2 ~2 ms; Kotlin for comparison https://github.com/dunefox/aoc-2021-in-kotlin/blob/main/src/Day7/day7.kt (~3 ms and 13 ms)

---
+++ 7/main.jl
@@ -0,0 +1,44 @@
+using BenchmarkTools, Test
+
+small_input = open("small_input.txt") do f
+    parse.(Int, split(readlines(f)[1], ","))
+end
+
+input = open("input.txt") do f
+    parse.(Int, split(readlines(f)[1], ","))
+end
+
+gauss(x::Int)::Int = (x^2 + x) / 2
+
+function part1(crabs::Array{Int})
+    _min = typemax(Int)
+
+    for crab in crabs
+        tmp = abs.(crabs .- crab) |> sum
+
+        if tmp < _min
+            _min = tmp
+        end
+    end
+
+    return _min
+end
+
+function part2(crabs)
+    _min = typemax(Int)
+
+    for crab in crabs
+        tmp = gauss.(abs.(crabs .- crab .- 1)) |> sum
+
+        if tmp < _min
+            _min = tmp
+        end
+    end
+
+    return _min
+end
+
+@test part1(small_input) == 37
+@test part1(input)
+@test part2(small_input) == 168
+@test part2(input)
